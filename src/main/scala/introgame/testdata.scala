1e41ed6dc5eeabe37010e2a82871a299dfe74e3a
Dyno Fu <dyno.fu@signifyd.com>
2018-08-20 09:13:08 -0700

    explicit coord in test data

--- src/main/scala/introgame/testdata.scala
+++ src/main/scala/introgame/testdata.scala
@@ -6,39 +6,39 @@ object testdata {
   val TestMap = GameMap[Cell](
     Vector(
       Vector( // 0
-        Cell("Haunted Oak Forest", "A spooky-looking forest filled with ancient oak trees.", RustySword :: Nil),
-        Cell("The Glades", "A grassy, wide open expanse that stretches for miles in every direction", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil)
+        Cell("(0, 0) Haunted Oak Forest", "A spooky-looking forest filled with ancient oak trees.", RustySword :: Nil),
+        Cell("(0, 1) The Glades", "A grassy, wide open expanse that stretches for miles in every direction", Nil),
+        Cell("(0, 2) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(0, 3) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(0, 4) Desert", "A barren expanse of nothingness.", Nil)
       ),
       Vector( // 1
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil)
+        Cell("(1, 0) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(1, 1) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(1, 2) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(1, 3) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(1, 4) Desert", "A barren expanse of nothingness.", Nil)
       ),
       Vector( // 2
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil)
+        Cell("(2, 0) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(2, 1) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(2, 2) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(2, 3) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(2, 4) Desert", "A barren expanse of nothingness.", Nil)
       ),
       Vector( // 3
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil)
+        Cell("(3, 0) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(3, 1) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(3, 2) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(3, 3) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(3, 4) Desert", "A barren expanse of nothingness.", Nil)
       ),
       Vector( // 4
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil),
-        Cell("Desert", "A barren expanse of nothingness.", Nil)
+        Cell("(4, 0) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(4, 1) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(4, 2) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(4, 3) Desert", "A barren expanse of nothingness.", Nil),
+        Cell("(4, 4) Desert", "A barren expanse of nothingness.", Nil)
       )
     ))
 }
